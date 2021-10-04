77685d8844167a3c06e0fc1353b888fd0084d384
KIRTIKUMARKK21 <kirtikumarkk21@gmail.com>
2021-10-05 01:54:56 +0530

    C++ Solution Of ProblemID_1672.cpp Added

--- C++/ProblemID_1672.cpp
+++ C++/ProblemID_1672.cpp
@@ -15,3 +15,22 @@ Time Complexity    Space Complexity
 
 */
 
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int answer=0;
+        for(int i=0;i<accounts.size();i++)
+        {
+            int sum=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                sum+=accounts[i][j];
+                
+            }
+            answer=max(answer,sum);
+        }
+        return answer;
+        
+    }
+};
+
